651b81c78c955d6db1777d68088835328303b899
ConnorWall <connorwall@u.boisestate.edu>
2019-12-08 18:43:52 -0700

    Added sort and add bookmark, fixed picker select on directions and settings page

--- src/components/Pages/Directions.js
+++ src/components/Pages/Directions.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react';
-import { SafeAreaView, View, Text, StyleSheet, ScrollView } from 'react-native';
+import { SafeAreaView, View, Text, StyleSheet, ScrollView, Picker, TouchableHighlight, Modal } from 'react-native';
 import { connect } from 'react-redux';
-import { Button, Card, SearchBar, ListItem } from 'react-native-elements';
+import { Button, Card, SearchBar, ListItem, } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import RNPickerSelect from 'react-native-picker-select';
+
+
+
 
 
 class DirectionsComp extends Component {
-  static navigationOptions = {
-    title: 'Directions',
-  };
+    static navigationOptions = {
+        title: 'Directions',
+    };
 
     state = {
         search: '',
+        sortSelect: 'Sort',
+        sorting: false,
+        modalVisible: false,
         dummyList: [
             {
                 title: 'Boise State Student Union',
@@ -41,87 +48,131 @@ class DirectionsComp extends Component {
                 id: '4',
                 favorite: false,
             },
-            {
-                title: 'Location',
-                leftIcon: 'close',
-                rightIcon: 'heart-outline',
-                id: '5',
-                favorite: false,
-            },
-            {
-                title: 'Location',
-                leftIcon: 'close',
-                rightIcon: 'heart-outline',
-                id: '6',
-                favorite: false,
-            },
-     
         ],
     };
-
-
     onChangeUserName() {
         console.log('Changed Text');
     }
-
     onButtonPress(pageName) {
         this.props.navigation.navigate(pageName);
     }
-
     addButtonPress() {
-        //adding bookmark code goes here
+        navigate('Add')
         console.log("Add bookmark pressed")
-
     }
-    sortButtonPress() {
-        //sorting bookmark code goes here
-        console.log('Sort bookmark pressed')
+    setModalVisible(visible) {
+        this.setState({ modalVisible: visible });
     }
-
     onDeleteBookmarkPressed(id) {
         var holderArray = this.state.dummyList;
         for (var i = 0; i < holderArray.length; i++) {
             var obj = holderArray[i];
             if (obj.id == id) {
                 holderArray.splice(i, 1);
+                obj.favorite = false;
             }
         }
         this.setState({ dummyList: holderArray });
-
     }
-
     onFavoriteBookmarkPressed(id) {
         var holderArray = this.state.dummyList;
-        
-            var obj = holderArray[id-1];
-        if (obj.rightIcon == 'heart-outline') {
-            obj.rightIcon = 'heart';
-            obj.favorite= true;
-
+        for (var i = 0; i < holderArray.length; i++) {
+            var obj = holderArray[i];
+            if (obj.id == id) {
+                if (obj.rightIcon == 'heart-outline') {
+                    obj.rightIcon = 'heart';
+                    obj.favorite = true;
+                }
+                else {
+                    obj.rightIcon = 'heart-outline'
+                    obj.favorite = false;
+                }
+                this.setState({ dummyList: holderArray });
+            }
         }
-        else {
-            obj.rightIcon = 'heart-outline'
-            obj.favorite = false;
-        }
-        this.setState({ dummyList: holderArray });
-        
     }
-
-
+    sortAlphabetically() {
+        var holderArray = this.state.dummyList;
+        holderArray.sort((a, b) => {
+            return a.title > b.title;
+        });
+        this.setState({ dummyList: holderArray });
+    }
     updateSearch = search => {
         this.setState({ search });
     };
-
     render() {
         const { search } = this.state;
+        const sortLevel = [
+            {
+                label: 'Sort:',
+                value: 'Sort',
+                description: 'Select an option to sort',
+            },
+            {
+                label: 'Alphabetically',
+                value: 'alphabetically',
+                description: 'Sort alphabetically',
+            },
+            {
+                label: 'Distance',
+                value: 'distance',
+                description:
+                    'Sort by distance',
+            },
+            {
+                label: 'Most Visited',
+                value: 'visited',
+                description:
+                    'Sort by most often visited',
+            },
+            {
+                label: 'Most Recent',
+                value: 'recent',
+                description:
+                    'Sort by most recently visited',
+            },
+        ];
         return (
             <SafeAreaView style={styles.containerStyles}>
                 <View style={styles.buttonContainer}>
-                    <Button title="Add Bookmark" buttonStyle={styles.buttonStyle} />
-                    <Button title="Sort Bookmarks" buttonStyle={styles.buttonStyle} />
+                    <Button title="Add Bookmark" buttonStyle={styles.buttonStyle} onPress={this.setModalVisible.bind(this, true)} />
+                    <RNPickerSelect
+                        style={pickerSelectStyles}
+                        items={sortLevel}
+                        onValueChange={value => {
+                            if (value == 'alphabetically') {
+                                this.sortAlphabetically()
+                            }
+                            //call other sort options here in future
+                            this.setState({
+                                sortSelect: value,
+                            });
+                        }}
+                        value={this.state.sortSelect}
+                        useNativeAndroidPickerStyle={false}
+                    />
+                </View>
+                <View style={{ marginTop: 22 }}>
+                    <Modal
+                        animationType="slide"
+                        transparent={false}
+                        visible={this.state.modalVisible}>
+                        <View style={{ marginTop: 220 }}>
+                            <View>
+                                <Text>Add a bookmark here.</Text>
+                                <TouchableHighlight onPress={() => {
+                                    this.setModalVisible(!this.state.modalVisible);
+                                }}>
+                                    <Text>Close</Text>
+                                </TouchableHighlight>
+                            </View>
+                        </View>
+
+                    </Modal>
                 </View>
                 <Card
-                    containerStyle={{ height: '50%' }}
+                    containerStyle={{ height: '80%' }}
                     wrapperStyle={styles.bookMarkContainer}>
                     <SearchBar
                         lightTheme
@@ -158,66 +209,16 @@ class DirectionsComp extends Component {
                     </ScrollView>
                 </Card>
             </SafeAreaView>
-            
-
-        );
-
-        return (
-            <SafeAreaView style={styles.containerStyles}>
-                <View style={styles.buttonContainer}>
-                    <Button title="Add Bookmark" buttonStyle={styles.buttonStyle} onPress={this.addButtonPress} />
-                    <Button title="Sort Bookmarks" buttonStyle={styles.buttonStyle} onPress={this.sortButtonPress} />
-                </View>
-                <Card>
-                    <SearchBar
-                        lightTheme
-                        placeholder="Search for a bookmark..."
-                        onChangeText={this.updateSearch}
-                        value={search}
-                    />
-                    {list.map((item, i) => (
-                        <ListItem
-                            key={i}
-                            title={item.title}
-                            leftIcon=
-                            {
-                                <Icon
-                                    size={26}
-                                    rasied
-                                    name={item.leftIcon}
-                                    type="material-community"
-                                    onPress={this.clearIconPress}
-                                />
-                            }
-
-                            bottomDivider
-                            rightIcon={
-                                <Icon
-                                    size={26}
-                                    rasied
-                                    name={item.rightIcon}
-                                    type="material-community"
-                                    onPress={this.favoriteIconPress}
-                                />
-                            }
-
-                        />
-                    ))}
-                </Card>
-            </SafeAreaView>
         );
     }
-
-
-
 }
-
 const styles = StyleSheet.create({
     containerStyles: {
         flex: 1,
         backgroundColor: '#E9E9EF',
         alignSelf: 'center',
         width: '100%',
+
     },
     buttonContainer: {
         flexDirection: 'row',
@@ -226,6 +227,7 @@ const styles = StyleSheet.create({
         alignSelf: 'center',
         width: '90%',
         height: '15%',
+
     },
     buttonStyle: {
         backgroundColor: '#FFA500',
@@ -242,12 +244,29 @@ const styles = StyleSheet.create({
         fontWeight: 'bold',
     },
 });
+const pickerSelectStyles = StyleSheet.create({
+    inputIOS: {
+        fontSize: 16,
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.5,
+        shadowRadius: 2,
+        elevation: 5,
+        paddingVertical: 12,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+        borderColor: 'gray',
+        backgroundColor: '#FFA500',
+        borderRadius: 4,
+        color: 'white',
+        paddingRight: 30,
+    }
+}
+)
+
 
 const mapStateToProps = state => {
     return { libraries: state.libraries };
 };
-
-
 const Directions = connect(mapStateToProps)(DirectionsComp);
-export {Directions};
-
+export { Directions };
